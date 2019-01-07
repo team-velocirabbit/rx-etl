d822fb3471059cc32d03a0617f1d5924e06a696a
Timothy Kachler <kachler@mac.com>
2019-01-07 13:40:02 -0800

    added mocha test files

---
+++ tests/etl.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const Etl = require('../Etl');
+
+
+describe('File: Etl.js', () => {
+  let testEtl;
+  beforeEach(() => {
+    const testFunc = function () { console.log('test'); };
+    testEtl = new Etl();
+    testEtl.transformers = [testFunc];
+  });
+  describe('Method: reset', () => {
+    it('Transformers length should equal zero', () => {
+      testEtl.reset();
+      expect(testEtl.transformers.length).to.equal(0);
+    });
+  });
+});
