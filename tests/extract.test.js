9248053ea6f516f98dcc82bb79c7f2420a402d29
Timothy Kachler <kachler@mac.com>
2019-01-07 16:28:53 -0800

    quick fix

--- tests/extract.test.js
+++ tests/extract.test.js
@@ -3,12 +3,6 @@ const Etl = require('../Etl');
 
 
 describe('File: Etl.js', () => {
-  let testEtl;
-  beforeEach(() => {
-    const testFunc = function () { console.log('test'); };
-    testEtl = new Etl();
-    testEtl.transformers = [testFunc];
-  });
   describe('Method: reset', () => {
     it('Transformers length should equal zero', () => {
       testEtl.reset();
